d78c54d273a3c045a01e68c9c8986a5c27327e88
duan55 <2233635634@qq.com>
2025-01-06 10:32:56 +0800

    p56 todoList的组件结构与样式划分

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
 import React, { Component } from 'react'
+import './App.css'
+import Header from './components/Header'
+import List from './components/List'
+import Footer from './components/Footer'
 
+//修改了class -> className && style="" -> style={{}}
 export default class App extends Component {
     render() {
         return (
-            <div>
-                App's content
+            <div className="todo-container">
+                <div className="todo-wrap">
+
+                    <Header />
+
+                    <List/>
+
+                    <Footer/>
+
+                </div>
             </div>
         )
     }
