e59eb674e05e64c9c7cf0625be9e1b4aa2b7664c
duan55 <2233635634@qq.com>
2025-01-23 16:38:35 +0800

    p68 axios发送请求ing

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -1,12 +1,23 @@
 import React, { Component } from 'react'
 
 export default class search extends Component {
+
+    //用户搜索
+    searchUser = () =>{
+        //1.获取用户的输入，button绑定了事件但是要获取input的输入需要使用ref、受控组件表单
+        //连续解构赋值直接获取到value并将其重命名为keyword，但是后文如果去取inputSth则会提示并没有解构得到inputSth
+        const {inputSth:{value:keyword}} = this
+        console.log(keyword)
+        //2.发送网络请求
+    }
+
     render() {
         return (
             <section className="jumbotron">
-                <h3 className="jumbotron-heading">Search Github Users</h3>
+                <h3 className="jumbotron-heading">搜索Github用户</h3>
                 <div>
-                    <input type="text" placeholder="enter the name you search" />&nbsp;<button>Search</button>
+                    <input ref={c => this.inputSth = c} type="text" placeholder="输入需要搜索的用户名" />&nbsp;
+                    <button onClick={this.searchUser}>搜索</button>
                 </div>
             </section>
         )
